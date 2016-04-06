6da3f22cc84c46206db2f9da27701bd2931c809c
Hackbright Student <info@hackbrightacademy.com>
2016-04-06 12:04:41 -0700

    This is the intial commit of the functions to do math

---
+++ arithmetic.py
@@ -0,0 +1,31 @@
+def add(num1, num2):
+    """Adds two numbers together"""
+    return num1 + num2
+
+def subtract(num1, num2):
+    """Subtracts num2 from num1"""
+    return num1 - num2
+
+def multiply(num1, num2):
+    """multiply num2 from num1"""
+    return num1 * num2
+
+def divide(num1, num2):
+    """divides num2 from num1"""
+    return num1/ num2
+
+def square(num1):
+    """Squares num1"""
+    return num1**2
+
+def cube(num1):
+    """cubes num1"""
+    return num1**3
+
+def power(num1, num2):
+    """Raises num1 to the power of num2"""
+    return num1**num2
+
+def mod(num1, num2):
+    """Returns the remainder when num1 is divided by num2"""
+    return num1 % num2
